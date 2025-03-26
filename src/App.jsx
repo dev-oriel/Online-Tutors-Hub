8aae16a92d730d8e13fd9e81c4bf5622a970e582
dev-oriel <kiplangatoriel@gmail.com>
2025-03-26 22:47:36 +0300

    Modified the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="bg-gray-100">
       <Navbar />
-      <HeroSection />
+      <Home />
       <Footer />
     </div>
   );
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,15 @@
+import hero from "../assets/images/hero.png";
+
 const HeroSection = () => {
   return (
-    <div className="bg-blue-600 text-white text-center py-16">
-      <h1 className="text-4xl font-bold">
-        Seamless Online Classes & Exams Help
-      </h1>
-      <p className="mt-4">
-        Get the best online tutors for your classes, assignments, and exams.
-      </p>
-      <a
-        href="#"
-        className="mt-6 inline-block bg-yellow-400 text-blue-800 font-semibold px-6 py-3 rounded-lg shadow-md hover:bg-yellow-500"
-      >
-        Get Started
-      </a>
-    </div>
+    <div
+      className="w-full h-screen bg-no-repeat bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${hero})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    ></div>
   );
 };
 
